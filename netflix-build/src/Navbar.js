671eb7d9c10d4b5defd677c8aebb3c13a2a1e8c8
AtomGen-2 <gaonkarsvedant.471@gmail.com>
2021-06-25 21:49:23 +0530

    phase 1-frontend

--- netflix-build/src/Navbar.js
+++ netflix-build/src/Navbar.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Navbar.css"
 
 function Navbar() {
+    const [show, handleShow] =useState(false);
+    const transitionNavbar = () => {
+        if (window.scrollY > 100){
+            handleShow(true);
+        }else{
+            handleShow(false)
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll", transitionNavbar);
+        return () => window.removeEventListener('scroll', transitionNavbar);
+    },[]);
+
     return (
-        <div className="nav nav__black">
+        <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
+
             <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" className="nav__logo"/>
             <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" className="nav__avatar"/>
             </div>
